41a854a3a39fad72869d53daeac2d67d08996c7f
AliseLoki <AliseLoki777@yandex.ru>
2026-03-30 09:21:48 +0300

    Locations

--- Assets/Intermediate/Scripts/EntryPoint.cs
+++ Assets/Intermediate/Scripts/EntryPoint.cs
@@ -5,14 +5,14 @@ public class EntryPoint : MonoBehaviour
     private const int WinScore = 5;
     private int _score;
 
-    [SerializeField] private PlayerController _player;
+    [SerializeField] private PlayerController _player;  // ссылки на все основные объекты в игре
     [SerializeField] private UIController _ui;
     [SerializeField] private CameraController _camera;
     [SerializeField] private EnemyController _enemy;
 
     private void OnEnable()
     {
-        _player.CollectablePicked += OnCollectablePicked;
+        _player.CollectablePicked += OnCollectablePicked;  // подписка на основные события в игре  -  сбор коллектэблов и если враг поймал
         _player.PlayerCatched += OnPlayerHasBeenCatched;
     }
 
@@ -24,17 +24,17 @@ public class EntryPoint : MonoBehaviour
 
     private void Awake()
     {
-        _camera.Init(_player.transform);
+        _camera.Init(_player.transform);  // инициализация всех приватных переменных в других скриптах
         _enemy.Init(_player.transform);
     }
 
-    private void OnPlayerHasBeenCatched()
+    private void OnPlayerHasBeenCatched() // вызов всех методов которые должны сработать если враг поймал игрока
     {
         _ui.ShowLoseText();
         StopGame();
     }
 
-    private void OnCollectablePicked()
+    private void OnCollectablePicked() // вызов всех методов которые должны сработать если игрок собрал коллектэбл
     {
         AddScore();
         _ui.UpdateText(_score);
